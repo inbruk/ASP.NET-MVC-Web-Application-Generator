b44966376482cae8fdde0b1d3f9cd4214ea36bc6
inbruk <inbruk@gmail.com>
2018-01-12 21:27:50 +0300

    структура конфигурации готова

---
+++ AspNetMvcWebApplicationGenerator.Configuration/OverallConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace AspNetMvcWebApplicationGenerator.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using AspNetMvcWebApplicationGenerator.Configuration.DataLayer;
+    using AspNetMvcWebApplicationGenerator.Configuration.BusinessLogicAndWebServicesLayer;
+    using AspNetMvcWebApplicationGenerator.Configuration.WebUserInterfaceLayer;
+
+    public class OverallConfiguration
+    {
+        public DLConfiguration   dlConfiguration { get; }
+        public BLWSConfiguration blwsConfiguration { get; }
+        public WUIConfiguration  wuiConfiguration { get; }
+
+        public OverallConfiguration
+        (
+            DLConfiguration   dlConf,
+            BLWSConfiguration blwsConf,
+            WUIConfiguration  wuiConf
+        )
+        {
+            dlConfiguration = dlConf;
+            blwsConfiguration = blwsConf;
+            wuiConfiguration = wuiConf;
+        }
+    }
+}
